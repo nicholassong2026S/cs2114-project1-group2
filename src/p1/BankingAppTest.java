7631a08469d492ae60d863715187f1fa27c206d9
Nicholas Song <nicholassong@vt.edu>
2026-09-21 13:30:49 -0400

    Adding Account & Banking Class + the tests

---
+++ src/p1/BankingAppTest.java
@@ -0,0 +1,18 @@
+package p1;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class BankingAppTest {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+    //~ Fields ................................................................
+
+    //~ Constructors ..........................................................
+
+    //~Public  Methods ........................................................
+
+}
