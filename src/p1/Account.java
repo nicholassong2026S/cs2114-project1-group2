6f63f4580525d6bde9bb0e3f5386359d0b994665
Hvitingur <LTStabler@gmail.com>
2026-09-21 15:29:51 -0400

    Copilot

--- src/p1/Account.java
+++ src/p1/Account.java
@@ -1,3 +1,5 @@
+package p1;
+
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 
--- src/p1/AccountTest.java
+++ src/p1/AccountTest.java
@@ -1,3 +1,4 @@
+package p1;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.time.Duration;
